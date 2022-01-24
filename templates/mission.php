089408489c883343deee6e96f34128439dc69e37
Aminul Islam <me@aminul.net>
2022-01-24 09:59:21 +0600

    branding

--- templates/mission.php
+++ templates/mission.php
@@ -4,6 +4,6 @@ style('privacy', 'privacy');
 ?>
 
 <div id="themes" class="section">
-	<h2><?php p($l->t('Your %s account', $theme->getName())) ?></h2>
-	<p><?php p($l->t('Your data represents who you are and can easily be abused. We at Nextcloud believe that you have a right to decide what happens with your data. Nextcloud protects your rights as defined in the User Data Manifesto!')); ?></p>
+	<h2><?php p($l->t('bitnext Secured Cloud')) ?></h2>
+	<p><?php p($l->t('Your data represents who you are and can easily be abused. We at bitnext Secured Cloud believe that you have a right to decide what happens with your data. bitnext Secured Cloud protects your rights as defined in the User Data Manifesto!')); ?></p>
 </div>
--- templates/who-has-access.php
+++ templates/who-has-access.php
@@ -1,7 +1,7 @@
 <div id="themes" class="section">
 	<h3><?php p($l->t('Who has access to your data?')) ?></h3>
-	<h4><?php p($l->t('Administrators')); ?></h4>
-	<div id="privacy_access_admins"></div>
+	<!-- <h4><?php p($l->t('Administrators')); ?></h4>
+	<div id="privacy_access_admins"></div> -->
 
 	<h4><?php p($l->t('People you shared with')) ?></h4>
 	<div id="privacy_access_shares"></div>
