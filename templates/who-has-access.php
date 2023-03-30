0bdf1c7e9f7480f5b503ac32375002bae02c357b
Aminul Islam <hosted.aminul@gmail.com>
2023-03-30 11:53:11 +0600

    bitnext

--- templates/who-has-access.php
+++ templates/who-has-access.php
@@ -1,7 +1,5 @@
 <div id="themes" class="section">
 	<h3><?php p($l->t('Who has access to your data?')) ?></h3>
-	<h4><?php p($l->t('Administrators')); ?></h4>
-	<div id="privacy_access_admins"></div>
 
 	<h4><?php p($l->t('People you shared with')) ?></h4>
 	<div id="privacy_access_shares"></div>
@@ -12,7 +10,4 @@
 		<a href="<?php print_unescaped($_['privacyPolicyUrl']) ?>"><?php p($l->t('Read the privacy policy.')) ?></a>
 	</p>
 	<?php endif; ?>
-
-	<h4><?php p($l->t('Encryption')) ?></h4>
-	<div id="privacy_access_encryption"></div>
 </div>
